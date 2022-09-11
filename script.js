3238eb4c927198de297c0d605907e294db8f77d2
Shiwang Pandey <110737840+ShiwangPande@users.noreply.github.com>
2022-09-11 23:07:07 +0530

    do it

---
+++ script.js
@@ -0,0 +1,34 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+function jump() {
+    if (dispatchEvent.classList != "jump") {
+        //first it checks if the dino is mid-jump. If not, it makes it jump.
+        dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump");
+            //removes the jump class from the dino once it has jumped so that it can jump again
+        }, 400);
+    }
+}
+let checkAlive = setInterval(function () {
+    let dinoTop = parseInt(
+        window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+    //check for collision
+    if (cactusLeft > 0 && cactusLeft < 70 && dinoTop >= 140) {
+        dino.style.animationPlayState = "paused";
+        cactus.style.animationPlayState = "paused";
+        alert("Whoops! Game Over :(");
+        window.location.reload();
+    }
+}, 10);
+document.addEventListener("keydown", function (event) {
+    jump();
+});
+document.addEventListener("click", function (event) {
+    jump();
+});
+{/* <b style="color: rgb(38, 38, 38); font-family: Poppins; font-size: x-large; --darkreader-inline-color:#d0ccc6;" data-darkreader-inline-color=""></b> */ }
